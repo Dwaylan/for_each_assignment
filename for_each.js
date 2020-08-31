12620b73f9de6bd604c18fc0900f17a5d373ce88
Dwaylan <Dwaylan.Applewhite@gmail.com>
2020-08-31 14:12:38 -0500

    added my first forEach function. More to come but comprehension plus notes are here

--- for_each.js
+++ for_each.js
@@ -3,12 +3,16 @@
 // forEach() calls a provided callback function once for each element in an array in ascending order.
 //  It is not invoked for index properties that have been deleted or are uninitialized.
 
-// callback is invoked with three arguments:
-// the value of the element
-// the index of the element
-// the Array object being traversed
 
-const forEachScratch = ['Mom', 'Dad', 'Grandma', 'Grandpa']
+const myRelatives = ['Mom', 'Dad', 'Grandma', 'Grandpa']
 // my scratch function is an array of string elements
+// console.log(myRelatives)
 
-forEachScratch.forEach(element => console.log( 'I miss' + ))
+myRelatives.forEach(function(myRelatives){
+    console.log('I really miss seeing ' + myRelatives)
+})
+// I have my "myRelatives" function and I use dot notation to invoke the forEach method
+// and I pass another function in the callback parenthesis.
+// I put "element" in the parameter preceeding the function in order to access the elements
+// in the array.
+// my foeEach function should print " I really miss seeing [element]"
