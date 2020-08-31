91a1becb557b1f4dbcb3982cc09edbeccdd13aa3
Dwaylan <Dwaylan.Applewhite@gmail.com>
2020-08-31 01:48:23 -0500

    the beginnings of the for each homework

---
+++ for_each.js
@@ -0,0 +1,14 @@
+'use strict'
+
+// forEach() calls a provided callback function once for each element in an array in ascending order.
+//  It is not invoked for index properties that have been deleted or are uninitialized.
+
+// callback is invoked with three arguments:
+// the value of the element
+// the index of the element
+// the Array object being traversed
+
+const forEachScratch = ['Mom', 'Dad', 'Grandma', 'Grandpa']
+// my scratch function is an array of string elements
+
+forEachScratch.forEach(element => console.log( 'I miss' + ))
